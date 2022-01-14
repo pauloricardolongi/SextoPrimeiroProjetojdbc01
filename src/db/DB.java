6917261b4e85d3cc459c1e74d5cbd9c02797d977
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-14 03:21:41 -0300

    jdbc02

--- src/db/DB.java
+++ src/db/DB.java
@@ -4,12 +4,15 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
 	
 	private static Connection conn = null;
+	
 	
 	public static Connection getConnection() {
 		if(conn == null) {
@@ -47,5 +50,26 @@ public class DB {
 			throw new DbException(e.getMessage());
 		}
 	}
+	public static void closeStatement(Statement st) {
+		if(st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	public static void closeResultSet(ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
 
 }
