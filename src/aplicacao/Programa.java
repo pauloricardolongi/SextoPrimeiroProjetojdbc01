1c7417608438e7f1c7b469650383fa6b7666c799
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-16 06:07:47 -0300

    transações

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -2,10 +2,13 @@ package aplicacao;
 
 import java.sql.Connection;
 
+
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
+import db.DbException;
 import db.DbIntegrityException;
 
 public class Programa {
@@ -13,24 +16,37 @@ public class Programa {
 	public static void main(String[] args) {
 		
 		Connection conn = null;
-		PreparedStatement st = null;
-		//ATUALIZAR SALÁRIOS
+		Statement st = null;
+		
 		try {
 			conn = DB.getConnection();
-			st =  conn.prepareStatement(
-					
-					"DELETE FROM department "
-					+ "WHERE "
-				    + "Id = ?");
-			st.setInt(1, 2);
 			
-					
-			int rowsAffected = st.executeUpdate();
+			conn.setAutoCommit(false);
 			
-			System.out.println("Done! Rows affected: " + rowsAffected);
+			st = conn.createStatement();
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+			
+			//int x = 1;
+			//if(x < 2) {
+			//	throw new SQLException("Fake error");
+			//}
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+			
+			conn.commit();
+			
+			System.out.println("rows1 " + rows1);
+			System.out.println("rows2 " + rows2);
 		}
 		catch(SQLException e) {
-			throw new DbIntegrityException(e.getMessage());
+			try {
+				conn.rollback();
+				throw new DbException("Transação não concluída! Causa por " + e.getMessage());
+				
+			} catch (SQLException e1) {
+				
+				throw new DbException("Erro trying to rollback! causa " + e1.getMessage());
+			   
+			}
 		}
 		finally {
 			DB.closeStatement(st);
